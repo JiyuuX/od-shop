3036324e9c63352ede5d8f9d91f2cf4e1ea9be89
JiyuuX <139239394+JiyuuX@users.noreply.github.com>
2024-07-13 19:03:26 +0300

    Update 22222 page.tsx

--- app/how-it-works/page.tsx
+++ app/how-it-works/page.tsx
@@ -14,7 +14,7 @@ const HowItWorks = () => {
         HOW IT WORKS?
       </motion.h1>
 
-      <div className="bg-[#181818] p-5 rounded-lg shadow-lg w-full max-w-3xl">
+      <div className="bg-[#181818] p-5 rounded-lg shadow-lg w-full max-w-xl md:max-w-3xl">
         <h2 className="text-2xl font-semibold mb-3 text-white">1) Choose Payment Method</h2>
         <p className="mb-4 text-gray-300 text-lg">
           You can select whether Crypto Payment or PayPal payment.
@@ -45,7 +45,7 @@ const HowItWorks = () => {
         <div className="mb-5">
           <h2 className="text-2xl font-semibold mb-3 text-white">2) Refund Policy:</h2>
           <p className="mb-2 text-gray-300">
-          Although it is unlikely to happen, sometimes the account can be banned by the companies. They seem not happy with the purchases caused by price differences from different countries <span role="img" aria-label="smile">😊</span>
+            Although it is unlikely to happen, sometimes the account can be banned by the companies. They seem not happy with the purchases caused by price differences from different countries <span role="img" aria-label="smile">😊</span>
           </p>
           <p className="text-gray-300">
             No problem, if your account becomes unusable - before expiration - your payment for the unused period will be refunded to your address. If you want, we can provide a new account for you. This choice is entirely up to you. We are OK for each solution! <span role="img" aria-label="heart">❤️</span>
@@ -60,7 +60,7 @@ const HowItWorks = () => {
         </div>
       </div>
 
-      <footer className="bg-[#0C0B0B] text-[#8E8484] p-2 text-center text-sm w-full mt-auto">
+      <footer className="bg-[#0C0B0B] text-[#8E8484] p-2 text-center text-sm w-full mt-5">
         &copy; 2024 - All rights reserved.
       </footer>
     </div>
